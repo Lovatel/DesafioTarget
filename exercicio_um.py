328550da6ee74f11f269eae5388688a12a2a2882
Gabriel Lovatel <gabriel.lovatel.martins@gmail.com>
2025-11-28 12:46:47 -0300

    exercise one fix

--- exercicio_um.py
+++ exercicio_um.py
@@ -4,7 +4,7 @@ COMMISSION_RATE_1 = 0.01
 COMMISSION_RATE_5 = 0.05
 
 def calculate_comission():
-    with open("vendas.json", "r+", encoding="utf8") as f:
+    with open("vendas.json", "r", encoding="utf-8") as f:
         data = json.load(f)
         for sales in data["vendas"]:
             if float(sales["valor"]) < 100:
@@ -13,8 +13,9 @@ def calculate_comission():
                 sales["comissão"] = round(sales["valor"] * COMMISSION_RATE_1, 2)
             elif float(sales["valor"]) >= 500:
                 sales["comissão"] = round(sales["valor"] * COMMISSION_RATE_5, 2)
-            
-        json.dump(data, f, indent= 4)
+        
+        with open("vendas.json", "w", encoding="utf8") as w:
+            json.dump(data, w, ensure_ascii=False, indent= 4)
             
         print(data)
 
